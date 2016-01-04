2da963824276eb411a75705da8524dc40e00d57f
sarunas.azna <sarunas.azna@gmail.com>
2016-01-04 21:57:56 +0200

    add key_layout_change file

---
+++ key_layout_change.sh
@@ -0,0 +1,9 @@
+lang=$(setxkbmap -query | grep layout |cut -c 13-15)
+echo $lang;
+
+if [ $lang == 'us' ]; then
+    setxkbmap lt
+elif [ $lang == 'lt' ]; then
+    setxkbmap us
+                
+fi
